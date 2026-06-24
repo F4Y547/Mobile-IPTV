fb020a682fc9a23d1a446b7720dda77c8f4bc8f3
F4Y547 <faysalmatheen@gmail.com>
2026-06-24 02:50:41 -0700

    feat: add Caze TV World Cup channel

---
+++ artifacts/syntv/src/data/extraChannels.ts
@@ -0,0 +1,10 @@
+import type { Channel } from "./channels";
+
+export const extraChannels: Channel[] = [
+  {
+    id: "caze-tv-fifa-world-cup-2026",
+    name: "Caze TV - FIFA World Cup 2026",
+    category: "Sports",
+    url: "https://dfr80qz435crc.cloudfront.net/MNOP/Amagi/Caze/Caze_TV_BR/1080p-vtt/index.m3u8",
+  },
+];
